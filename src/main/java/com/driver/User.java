c2fdfabbf846eefbc4590d2117ae85a6f39ae2bd
ayushi-mishra0 <msseema007@gmail.com>
2023-07-22 22:02:47 +0530

    done

--- src/main/java/com/driver/User.java
+++ src/main/java/com/driver/User.java
@@ -1,6 +1,40 @@
 package com.driver;
 
 public class User {
+
     private String name;
     private String mobile;
+    private boolean isAdmin;
+
+    public boolean isAdmin() {
+        return isAdmin;
+    }
+
+    public void setAdmin(boolean admin) {
+        isAdmin = admin;
+    }
+
+    public User(String name, String mobile) {
+        this.name = name;
+        this.mobile=mobile;
+    }
+
+    public User() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
 }
